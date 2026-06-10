b4c5f0fe57320bdb712470990e9e4499bb278ec3
dharani834 <dudedharani4@gmail.com>
2026-06-10 16:47:38 +0530

    message

--- app.py
+++ app.py
@@ -11,30 +11,36 @@ def home():
     return render_template("home.html")
 
 @app.route('/predict',methods=["POST"])
+
+@app.route('/predict', methods=["POST"])
 def predict():
     try:
+        data = request.get_json(force=True)
+
         features = [
-    int(request.form['no_of_dependents']),
-    int(request.form['education']),
-    int(request.form['self_employed']),
-    int(request.form['income_annum']),
-    int(request.form['loan_amount']),
-    int(request.form['loan_term']),
-    int(request.form['cibil_score']),
-    int(request.form['residential_assets_value']),
-    int(request.form['commercial_assets_value']),
-    int(request.form['luxury_assets_value']),
-    int(request.form['bank_asset_value'])
-    ]
-        input=np.array([features])
-        prediction=model.predict(input)[0]
-        if prediction==1:
-            result="Approval"
-        else:
-            result="Rejected"
-        return render_template("home.html",prediction_text=result)
+            data['no_of_dependents'],
+            data['education'],
+            data['self_employed'],
+            data['income_annum'],
+            data['loan_amount'],
+            data['loan_term'],
+            data['cibil_score'],
+            data['residential_assets_value'],
+            data['commercial_assets_value'],
+            data['luxury_assets_value'],
+            data['bank_asset_value']
+        ]
+
+        input_data = np.array([features])
+
+        prediction = model.predict(input_data)[0]
+
+        result = "Approval" if prediction == 1 else "Rejected"
+
+        return {"prediction": result}
+
     except Exception as e:
-        return str(e)
+        return {"error": str(e)}
     
 if __name__=="__main__":
     app.run(debug=True)
